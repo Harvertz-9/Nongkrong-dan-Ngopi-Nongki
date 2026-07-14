e309546fe559ccddfd2e41876411883b13f647ee
Harvertz-9 <thismyproject25t@gmail.com>
2026-07-14 10:37:37 +0800

    add vue.js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,9 @@
-<!doctype html>
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    @vite('resources/css/app.css')
-  </head>
-  <body>
-    <h1 class="text-5xl font-bold underline font-Poppins">
-      Hello world!
-    </h1>
-  </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <div id="app"></div>
+</body>
 </html>
